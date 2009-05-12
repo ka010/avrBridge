df1666565d2f582001279018182d2f69e8f70262
ka010 <kai.aras@freenet.de>
2009-05-12 19:13:27 +0200

    update

---
+++ avrBridgeJava/src/avrBridgeJavaDev/graph/widgets/pinWidget.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package avrBridgeJavaDev.graph.widgets;
+
+/**
+ *
+ * @author blahlol
+ */
+public class pinWidget {
+
+}
